92c49cbb4424a386b931c183ec05c5e196278f7c
jessicagrunewald <j_grunewald@hotmail.com>
2024-04-26 10:01:52 +1200

    quantity of product sold per store

--- Sales.py
+++ Sales.py
@@ -10,3 +10,7 @@ pd.set_option('display.max_columns', None)
 # revenue per store
 # filtering 'ID Loja' and 'Valor Final' columns and group data by store
 revenue = db_sales[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+
+# quantity of product sold per store
+# filtering 'ID Loja' and 'Quantidade' columns and group data by store
+quantity_product = db_sales[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()

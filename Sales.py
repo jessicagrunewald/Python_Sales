52490d317102fd22ffd6484db112db54cbc7dc10
jessicagrunewald <j_grunewald@hotmail.com>
2024-04-26 09:54:36 +1200

    revenue per store

--- Sales.py
+++ Sales.py
@@ -6,3 +6,7 @@ db_sales = pd.read_excel('Sales.xlsx')
 
 # visualization of database data
 pd.set_option('display.max_columns', None)
+
+# revenue per store
+# filtering 'ID Loja' and 'Valor Final' columns and group data by store
+revenue = db_sales[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
